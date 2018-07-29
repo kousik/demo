d6b0305a69d1a1258f6d49edbed1c6f8e5e5a573
Kousik Das <kousik.das@outlook.com>
2018-07-29 17:23:38 +0530

    few updates

--- wp-content/plugins/frontend-dashboard/_inc/js/community.js
+++ wp-content/plugins/frontend-dashboard/_inc/js/community.js
@@ -15,6 +15,34 @@
  *   http://www.gnu.org/licenses/gpl.html
  
  */
+(function($) {
+    $(window).on('load resize', function() {
+        $('.full-image-slider').each(function() {
+            et_fullscreen_slider($(this));
+        });
+    });
+    function et_fullscreen_slider(et_slider) {
+        var et_viewport_height = $(window).height(),
+            et_slider_height = $(et_slider).find('.et_pb_slider_container_inner').innerHeight(),
+            $admin_bar = $('#wpadminbar'),
+            $main_header = $('#main-header'),
+            $top_header = $('#top-header');
+        $(et_slider).height('auto');
+        if ($admin_bar.length) {
+            var et_viewport_height = et_viewport_height - $admin_bar.height();
+        }
+        if ($top_header.length) {
+            var et_viewport_height = et_viewport_height - $top_header.height();
+        }
+        if (!$('.et_transparent_nav').length && !$('.et_vertical_nav').length) {
+            var et_viewport_height = et_viewport_height - $main_header.height();
+        }
+        if (et_viewport_height > et_slider_height) {
+            $(et_slider).height(et_viewport_height);
+        }
+    }
+})(jQuery);
+
 
 (function ($) {
     //  inspired by DISQUS
